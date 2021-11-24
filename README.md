75bfced25f0013ebeded84dadfbd9f4f68d9d5ae
Sam Page <sampage6@gmail.com>
2021-11-24 00:30:40 -0600

    finished functionality

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# generate-readme
-An automatic readme generator created from input through node.js
+# Yes mang
+  Description:
+  This is the yes mang production... it is evolved from the yes boi production
+
+  Installation:
+  to install, give it a firm whack on the buns
+
+  Usage:
+  for rachel profiling
+
+  Contributing:
+  yes! follow these guidelines to avoid being labeled a rachel profiler
+
+  Tests:
+  nope!
+
+  ---
+
+  
+
---
+++ index.js
@@ -0,0 +1,55 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs')
+const generateMarkdown = require('./generateMarkdown')
+// TODO: Create an array of questions for user input
+
+const questions = [{
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
+},
+{   type: 'input',
+    name: 'description',
+    message: 'Give a description of your project.'
+},
+{   type: 'input',
+    name: 'installation',
+    message: 'Please explain the installation instructions for your project.'
+},
+{   type: 'input',
+    name: 'usage',
+    message: 'What is this project used for?',
+},
+{   type: 'input',
+    name: 'contributing',
+    message: 'Are there any guidelines you would like contributors to follow?'
+},
+{   type: 'input',
+    name: 'tests',
+    message: 'Are there any test instructions you would like your contributors to follow?'
+},
+{   type: 'select',
+    name: 'contributing',
+    message: 'Choose a license.'
+}];
+
+// TODO: Create a function to write README file
+function writeToFile(data) {
+    const makeReadme = generateMarkdown(data)
+    fs.writeFile('README.md', makeReadme, (err) =>
+    err ? console.log(err) : console.log('Created your README.md')
+    );
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then((data) => {
+        writeToFile(data)
+    }
+
+    )}
+
+// Function call to initialize app
+init();
