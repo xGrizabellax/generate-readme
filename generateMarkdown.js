3d4402ddf8b2e6976c21fc9d2fd1237d595fbc5c
Sam Page <sampage6@gmail.com>
2021-11-24 02:12:59 -0600

    test 2 of MIT badge

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -38,7 +38,7 @@ function generateMarkdown(data) {
 
   # License
 
-  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]
 
 
 
