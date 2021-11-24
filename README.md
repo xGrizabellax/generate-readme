9c65bf6a33e03f67c7ad210a4d5ba486a74aa7c4
Sam Page <sampage6@gmail.com>
2021-11-24 04:01:21 -0600

    cleaned code and made it operational

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-# lsjadf;kjf;l ![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)
+# jflsk;adfasf ![License: BSD](<https://img.shields.io/badge/License-BSD_3--Clause-blue.svg>)
   ### Description:
-  jkfskdfhkjdsfjksdh
+  sjadh;sljfkhsdalk
 
   ### Installation:
-  fjksdakhjlkhflksa
+  kjlsahfjksdhaf
 
   ### Usage:
-  jksahfjkldhslfk
+  dsjkhfajksldfh
 
   ### Contributing:
-  safkjdlhfjkldsh
+  skajhflkdsjafh
 
   ### Tests:
-  skajflhsjkdfh
+  jsahdbf
 
   ### Questions:
 
@@ -21,7 +21,7 @@
   ___
 
   # License
-  [Apache 2.0 License](https://opensource.org/licenses/Apache-2.0)
+  [BSD (3-Clause) License](https://opensource.org/licenses/BSD-3-Clause)
 
 
 
--- generateMarkdown.js
+++ generateMarkdown.js
@@ -2,11 +2,11 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === 'MIT') {
-  return `![License:${license}](<https://img.shields.io/badge/license-MIT-yellow.png>)`
+  return `![License: MIT](<https://img.shields.io/badge/License-MIT-yellow.svg>)`
 } else if (license === 'Apache 2.0') {
-  return `![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`
+  return `![License: Apache 2.0](<https://img.shields.io/badge/License-Apache_2.0-blue.svg>)`
 } else if (license === 'BSD') {
-  return `![License:${license}](<https://img.shields.io/badge/license-BSD-yellow.png>)`
+  return `![License: BSD](<https://img.shields.io/badge/License-BSD_3--Clause-blue.svg>)`
 }
 }
 
